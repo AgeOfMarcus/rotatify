56c556410abeee367e181fe30452daad389cb625
Marcus Weinberger <marcusjw.ftw@gmail.com>
2022-04-27 15:17:14 -0400

    add heroku files

---
+++ setup.sh
@@ -0,0 +1,12 @@
+# setup for heroku
+mkdir -p ~/.streamlit/
+echo "
+[general]n
+email = "your-email@domain.com"n
+" > ~/.streamlit/credentials.toml
+echo "
+[server]n
+headless = truen
+enableCORS=falsen
+port = $PORTn
+" > ~/.streamlit/config.toml
